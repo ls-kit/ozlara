e3d8a832ea2e1ca79bd35e06f9124c584b811f15
nsakib9 <sakibkhan207@gmail.com>
2021-10-03 11:31:42 +0600

    first start here

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,9 @@
     <!-- This is an example component -->
  <div id="wrapper" class="container px-4 py-4 mx-auto">
 
-    @if (!$settings->activated)
+    {{-- @if (!$settings->activated) --}}
         @include('partials.activate-modal')
-    @endif
+    {{-- @endif --}}
      <div class="sm:grid sm:h-32 sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
 
         <x-status type="positive" title="Today's wishists" number="32" growth="9"/>
